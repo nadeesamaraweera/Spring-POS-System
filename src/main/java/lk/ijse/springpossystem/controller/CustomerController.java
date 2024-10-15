8df51a46a0f5c32736c5c7be9bea008deb9c5e23
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-10-15 13:06:34 +0530

    update delete customer

--- src/main/java/lk/ijse/springpossystem/controller/CustomerController.java
+++ src/main/java/lk/ijse/springpossystem/controller/CustomerController.java
@@ -81,5 +81,17 @@ public class CustomerController {
         return customerService.getAllCustomers();
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable("id") String customerId) {
+        try {
+            customerService.deleteCustomer(customerId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (CustomerNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 
 }
